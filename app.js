43a1bd1b5fdbf16db1686c5d008dab81e072a102
Charles <bernaldezsay@gmail.com>
2020-05-27 09:20:37 +0800

    added new features and tweaks

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 new Vue({
   el: '#app',
   data: {
-    newList: '',
-    lists: []
+    addTodo: '',
+    lists: [
+      {text: "Clean the room", isDone: false},
+      {text: "Cook food", isDone: false},
+      {text: "Study Vue", isDone: true},
+      {text: "Sleep", isDone: true},
+    ] 
   },
 
   methods: {
     addList() {
-      this.lists.push(this.newList);
-      this.newList = '';
+      this.lists.push({text: this.addTodo, isDone: false});
+      this.addTodo = '';
     },
 
     removeList(remove) {
       this.$delete(this.lists, remove);
+    },
+
+    toggleDone(list) {
+      list.isDone = !list.isDone;
     }
   }
 });
