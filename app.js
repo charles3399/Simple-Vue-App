d7930c069a3a78696e37012d3eda7db1ea6723a2
Charles <bernaldezsay@gmail.com>
2020-05-29 19:15:54 +0800

    Added a Vue component in main.js

--- app.js
+++ app.js
@@ -1,13 +1,54 @@
-new Vue({
-  el: '#app',
-  data: {
-    addTodo: '',
+Vue.component('todolists', {
+  template: `
+    <div>
+      <div class="card text-center shadow my-5">
+        <div class="card-header"><h4>List your to-do for today!</h4></div>
+        <div class="card-body">
+        
+          <input class="form-control" type='text' v-model="addTodo" v-on:keyup.enter="addList">
+
+          <button class="btn btn-secondary my-2" @click='addList' :disabled='addTodo.length === 0'>
+            <i class="fa fa-plus-circle" aria-hidden="true"><strong> Add</strong></i>
+          </button>
+
+        </div>
+      </div>
+
+      <ul class="list-group">
+          <li class="shadow mb-4 list-group-item animate__animated animate__fadeInDown"
+          v-for="list, remove in lists">
+
+            <span v-if="list.isDone"><s>{{list.text}}</s></span>
+            <span v-else>{{list.text}}</span>
+
+            <button type="button" class="close float-right" aria-label="Close" @click="removeList(remove)">
+              <span aria-hidden="true">&times;</span>
+            </button>
+
+            <button v-if="list.isDone" class="btn undoButton float-right" @click="toggleDone(list)">
+              <i class="fas fa-undo" aria-hidden="true"><strong> Undo</strong></i> 
+            </button>
+
+            <button v-else class="btn doneButton float-right" @click="toggleDone(list)">
+              <i class="far fa-check-circle" aria-hidden="true"><strong> Mark as done</strong></i> 
+            </button>
+
+          </li>
+
+      </ul>
+    </div>  
+  `,
+
+  data() {
+    return {
+      addTodo: '',
     lists: [
       {text: "Clean the room", isDone: false},
       {text: "Cook food", isDone: false},
       {text: "Study Vue", isDone: true},
       {text: "Sleep", isDone: true},
     ] 
+    }
   },
 
   methods: {
@@ -24,4 +65,9 @@ new Vue({
       list.isDone = !list.isDone;
     }
   }
+
+});
+
+new Vue({
+  el: '#app'
 });
